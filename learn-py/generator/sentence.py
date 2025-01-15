16cd91b47d429333c139d65e92e6c8792dc41ae5
igaozp <igaozp@hotmail.com>
2025-01-15 22:17:12 +0800

    feat: add sentence iterator and generator classes with corresponding tests

--- learn-py/generator/sentence.py
+++ learn-py/generator/sentence.py
@@ -3,6 +3,7 @@ import reprlib
 
 RE_WORD = re.compile(r'\w+')
 
+
 class Sentence:
     def __init__(self, text):
         self.text = text
@@ -16,3 +17,56 @@ class Sentence:
 
     def __repr__(self):
         return 'Sentence(%s)' % reprlib.repr(self.text)
+
+
+class SentenceIterator:
+    def __init__(self, text):
+        self.words = RE_WORD.findall(text)
+        self.index = 0
+
+    def __next__(self):
+        try:
+            word = self.words[self.index]
+        except IndexError:
+            raise StopIteration()
+        self.index += 1
+        return word
+
+    def __iter__(self):
+        return self
+
+
+class SentenceGenerator:
+    def __init__(self, text):
+        self.text = text
+        self.words = RE_WORD.findall(text)
+
+    def __repr__(self):
+        return 'SentenceGenerator(%s)' % reprlib.repr(self.text)
+
+    def __iter__(self):
+        for word in self.words:
+            yield word
+
+
+class SentenceLazyGenerator:
+    def __init__(self, text):
+        self.text = text
+
+    def __repr__(self):
+        return 'SentenceLazyGenerator(%s)' % reprlib.repr(self.text)
+
+    def __iter__(self):
+        for match in RE_WORD.finditer(self.text):
+            yield match.group()
+
+
+class SentenceGeneratorExp:
+    def __init__(self, text):
+        self.text = text
+
+    def __repr__(self):
+        return 'SentenceGeneratorExp(%s)' % reprlib.repr(self.text)
+
+    def __iter__(self):
+        return (match.group() for match in RE_WORD.finditer(self.text))
--- learn-py/tests/generator/sentence.py
+++ learn-py/tests/generator/sentence.py
@@ -3,8 +3,36 @@ from generator import sentence
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
+    def test_sentence():
         s = sentence.Sentence('"The time ha... Walrus said,')
+        for word in s:
+            print(word)
+        print(list(s))
+
+    def test_sentence_iter(self):
+        s = sentence.SentenceIterator('"The time ha... Walrus said,')
+        for word in s:
+            print(word)
+        # print nothing
+        print(list(s))
+        # print list by reset iterator
+        s = sentence.SentenceIterator('"The time ha... Walrus said,')
+        print(list(s))
+
+    def test_sentence_generator(self):
+        s = sentence.SentenceGenerator('"The time ha... Walrus said,')
+        for word in s:
+            print(word)
+        print(list(s))
+
+    def test_sentence_lazy_generator(self):
+        s = sentence.SentenceLazyGenerator('"The time ha... Walrus said,')
+        for word in s:
+            print(word)
+        print(list(s))
+
+    def test_sentence_generator_exp(self):
+        s = sentence.SentenceGeneratorExp('"The time ha... Walrus said,')
         for word in s:
             print(word)
         print(list(s))
